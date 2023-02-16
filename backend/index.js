18097016c9cd05fd821198faf416f4d11dfd29f1
eminkartci <amean.kartci@gmail.com>
2023-02-16 22:24:33 +0300

    bakcend ile bağlandı

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+var cors = require('cors')
 const dotenv = require('dotenv');
 const app = express()
+
+app.use(cors())
 
 dotenv.config()
 const PORT = process.env.PORT || 3006;
@@ -20,7 +23,10 @@ app.get("/urunler", (req,res) => {
             {id: 1, isim: "Elbise", fiyat: 55, gorsel: "albise1.webp"},
             {id: 2, isim: "Bilgisayar", fiyat:10000},
             {id: 3, isim: "Araba", fiyat:350000},
-            {id: 4, isim: "Plak", fiyat:320}
+            {id: 4, isim: "Plak", fiyat:320},
+            {id: 5, isim: "Danışmanlık", fiyat:1000},
+            {id: 6, isim: "Canlı Müzik", fiyat:350},
+            {id: 7, isim: "Telefon", fiyat:12000,gorsel: "telefon.jpeg"},
           ]
     })
 })
