05e75ed591a495474325c58334f20e1f83a5fde1
eminkartci <amean.kartci@gmail.com>
2023-02-16 22:14:41 +0300

    Header düzenlendi

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -14,7 +14,28 @@ function Header() {
         />
         <link rel="icon" href="/AMEAN Logo Golgeli.png" />
       </Head>
-      <div className={styles.container}>Header ÖDEVİ</div>
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>A=pp</div>
+
+        <div className={styles.navlinksContainer}>
+          <div className={styles.navLinks}>
+            <div className={styles.link}> Link 1</div>
+            <div className={styles.link}> Link 2</div>
+            <div className={styles.link}> Link 3</div>
+            <div className={styles.link}> Link 4</div>
+            <div className={styles.link}> Link 5</div>
+          </div>
+
+          <div className={styles.profileContainer}>
+            <img
+              src="/profileimage.webp"
+              alt="Profil resmi"
+              className={styles.profileImg}
+            />
+            <p> İsim Soyisim</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
